eef842b6c66824d9e00a33b7a4e70fd1c625f2c2
kiriasru <kiriasru@gmail.com>
2025-08-09 11:19:00 -0600

    Moto class created

---
+++ SistemaVehiculos/Moto.cs
@@ -0,0 +1,19 @@
+﻿using SistemaVehiculos.Models;
+
+namespace SistemaVehiculos;
+
+public class Moto : Vehiculo
+{
+    // Atributos para Moto
+    public int Cilindrada { get; set; } // en cc
+    public bool TieneMaletero { get; set; }
+    // Metodos
+    public override void MostrarInformacion()
+    {
+        base.MostrarInformacion();
+        Console.WriteLine($"Cilindrada: {Cilindrada} cc");
+        Console.WriteLine($"Maletero: {(TieneMaletero ? "Si" : "No")}");
+        Console.WriteLine();
+    } // Fin de MostrarInformacion (para moto)
+    
+} // Fin de class
