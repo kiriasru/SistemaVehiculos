830008ae8a58ef122f082caf165b443a5ecb6818
kiriasru <kiriasru@gmail.com>
2025-08-09 11:17:51 -0600

    Abstract class created

---
+++ SistemaVehiculos/Models/Vehiculo.cs
@@ -0,0 +1,19 @@
+﻿namespace SistemaVehiculos.Models;
+
+public class Vehiculo
+{   
+    // Atributos para Vehiculo
+    public string Marca { get; set; }
+    public string Modelo { get; set; }
+    public int Anio { get; set; }
+    
+    // Metodos
+    public virtual void MostrarInformacion()
+    {
+        Console.WriteLine("----- Información del vehículo -----");
+        Console.WriteLine($"Marca: {Marca}");
+        Console.WriteLine($"Modelo: {Modelo}");
+        Console.WriteLine($"Año: {Anio}");
+    } // Fin de MostrarInformacion
+    
+} // Fin de class
