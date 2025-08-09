8a2bf014015fad199dd1efec8b75e13fd97cd524
kiriasru <kiriasru@gmail.com>
2025-08-09 11:18:53 -0600

    Carro Class created

---
+++ SistemaVehiculos/Carro.cs
@@ -0,0 +1,19 @@
+﻿using SistemaVehiculos.Models;
+
+namespace SistemaVehiculos;
+
+public class Carro : Vehiculo
+{
+    // Atributos para Carro
+    public int NumeroPuertas { get; set; }
+    public string TipoCombustible { get; set; }
+    // Metodos
+    public override void MostrarInformacion()
+    {
+        base.MostrarInformacion();
+        Console.WriteLine($"Número de puertas: {NumeroPuertas}");
+        Console.WriteLine($"Tipo de combustible: {TipoCombustible}");
+        Console.WriteLine();
+    } // Fin de MostrarInformacion (para carro)
+    
+} // Fin de class
